8be8b18fdb5d1bd18eb1c9dc3a6bfd94eab7e887
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-06-15 22:29:17 +0300

    Update MyInjectEventApp, misc Injector refactoring and pull request #6 review. removed unneeded android.jar library

--- src/com/github/xsavikx/android/screencast/api/injector/SimpleResultCheckMultiLineReceiver.java
+++ src/com/github/xsavikx/android/screencast/api/injector/SimpleResultCheckMultiLineReceiver.java
@@ -3,37 +3,30 @@ package com.github.xsavikx.android.screencast.api.injector;
 import com.android.ddmlib.MultiLineReceiver;
 
 public class SimpleResultCheckMultiLineReceiver extends MultiLineReceiver {
-    private String successStrPtn;
-    private String failureStrPtn;
-    private boolean isSucceed;
+  private String successPattern;
+  private boolean isSucceed;
 
-    public SimpleResultCheckMultiLineReceiver(String successStrPtn, String failureStrPtn) {
-        this.successStrPtn = successStrPtn;
-        // failure check is not done right now
-        this.failureStrPtn = failureStrPtn;
-        // default is false, you MUST pass a correct successStrPtn
-        this.isSucceed = false;
+  public SimpleResultCheckMultiLineReceiver(String successPattern) {
+    this.successPattern = successPattern;
+    this.isSucceed = false;
+  }
+
+  @Override
+  public boolean isCancelled() {
+    return false;
+  }
+
+  @Override
+  public void processNewLines(String[] arg0) {
+    for (String elem : arg0) {
+      if (!successPattern.isEmpty() && elem.indexOf(successPattern) > 0) {
+        isSucceed = true;
+      }
+      System.out.println(elem);
     }
+  }
 
-
-    @Override
-    public boolean isCancelled() {
-        return false;
-    }
-
-    @Override
-    public void processNewLines(String[] arg0) {
-        for (String elem : arg0) {
-
-            if(!successStrPtn.isEmpty() && elem.indexOf(successStrPtn) != -1) {
-                isSucceed = true;
-            }
-
-            System.out.println(elem);
-        }
-    }
-
-    public boolean isSucceed() {
-        return isSucceed;
-    }
+  public boolean isSucceed() {
+    return isSucceed;
+  }
 }
